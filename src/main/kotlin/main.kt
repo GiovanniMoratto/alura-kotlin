426ae8f1eafd10190275fd51462ad32014624033
Giovanni Moratto <morattotrabalho@gmail.com>
2021-08-06 16:10:37 -0300

    fun main v1.3

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,15 +2,30 @@
  *@Author giovanni.moratto
  */
 
-fun main(){
+fun main() {
     println("Bem vindo ao Bytebank")
     val titular: String = "Giovanni"
     val numeroConta: Int = 1000
     var saldo = 0.0
     saldo = 100 + 2.0
     saldo += 200
+    saldo -= 1000
 
     println("titular $titular")
     println("número da conta $numeroConta")
     println("saldo da conta $saldo")
+
+    if (saldo > 0.0) {
+        println("conta é positiva")
+    } else if (saldo == 0.0) {
+        println("conta é neutra")
+    } else {
+        println("conta é negativa")
+    }
+
+    when {
+        saldo > 0.0 -> println("conta é positiva")
+        saldo == 0.0 -> println("conta é neutra")
+        else -> println("conta é negativa")
+    }
 }
