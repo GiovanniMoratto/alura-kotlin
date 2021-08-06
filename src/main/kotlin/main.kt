290486cdffad1160306b4733dbc4ddcf666d8298
Giovanni Moratto <morattotrabalho@gmail.com>
2021-08-06 16:47:12 -0300

    fun main v2.1

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -7,30 +7,67 @@ fun main() {
     val contaGiovanni = Conta()
     contaGiovanni.titular = "Giovanni"
     contaGiovanni.numero = 1000
-    contaGiovanni.setSaldo(-200.0)
+    contaGiovanni.deposita(200.0)
 
     val conta2 = Conta()
     conta2.titular = "2"
     conta2.numero = 1001
-    conta2.setSaldo(300.0)
+    conta2.deposita(300.0)
 
     println(conta2.titular)
     println(conta2.numero)
-    println(conta2.getSaldo())
+    println(conta2.saldo)
 
     println(contaGiovanni.titular)
     println(contaGiovanni.numero)
-    println(contaGiovanni.getSaldo())
+    println(contaGiovanni.saldo)
 
+    println("depositando na conta do Giovanni")
+    contaGiovanni.deposita(50.0)
+    println(contaGiovanni.saldo)
+
+    println("depositando na conta 2")
+    conta2.deposita(70.0)
+    println(conta2.saldo)
+
+    println("sacando na conta do Giovanni")
+    contaGiovanni.saca(250.0)
+    println(contaGiovanni.saldo)
+
+    println("sacando na conta 2")
+    conta2.saca(100.0)
+    println(conta2.saldo)
+
+    println("saque em excesso na conta do Giovanni")
+    contaGiovanni.saca(100.0)
+    println(contaGiovanni.saldo)
+
+    println("saque em excesso na conta 2")
+    conta2.saca(500.0)
+    println(conta2.saldo)
+
+    println("Transferência da conta 2 para o Giovanni")
+
+    if (conta2.transfere(300.0, contaGiovanni)) {
+        println("Transferência sucedida")
+    } else {
+        println("Falha na transferência")
+    }
+
+    println(contaGiovanni.saldo)
+    println(conta2.saldo)
 }
 
 class Conta {
     var titular = ""
     var numero = 0
-    private var saldo = 0.0
+    var saldo = 0.0
+        private set
 
     fun deposita(valor: Double) {
-        this.saldo += valor
+        if (valor > 0) {
+            this.saldo += valor
+        }
     }
 
     fun saca(valor: Double) {
@@ -46,16 +83,6 @@ class Conta {
             return true
         }
         return false
-    }
-
-    fun getSaldo(): Double {
-        return saldo
-    }
-
-    fun setSaldo(valor: Double) {
-        if (valor > 0) {
-            saldo = valor
-        }
     }
 }
 
